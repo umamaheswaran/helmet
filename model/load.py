72daf7e82d5fb1a8cdf76fbf291820aca79889df
uma <u.umaesh@gmail.com>
2018-10-30 23:51:59 +0530

    added pretrained model

--- model/load.py
+++ model/load.py
@@ -1,22 +1,25 @@
 import numpy as np
 import keras.models
 from keras.models import model_from_json
-from scipy.misc import imread, imresize,imshow
 import tensorflow as tf
+from keras.utils.data_utils import get_file
 
-
+            
 def init(): 
-	json_file = open('model.json','r')
+	json_file = open('model/classifier.json','r')
 	loaded_model_json = json_file.read()
 	json_file.close()
 	loaded_model = model_from_json(loaded_model_json)
 
 	#load woeights into new model
-	loaded_model.load_weights("model.h5")
-	print("Loaded Model from disk")
+	
+	loaded_model.load_weights("classifier_weights.h5")
+	print(weights_path)
+	print("Loaded Model")
 
 	#compile and evaluate loaded model
 	loaded_model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 	graph = tf.get_default_graph()
 
 	return loaded_model,graph
+
